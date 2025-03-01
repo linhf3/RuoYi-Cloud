7b2343ae0b5caaf7e5a003c3607508b248284a52
linhf3 <191663059@qq.com>
2025-03-01 11:09:59 +0800

    初始化代码

---
+++ ruoyi-modules/ruoyi-securities/src/main/java/com/ruoyi/securities/service/ITbSecuritiesDataService.java
@@ -0,0 +1,61 @@
+package com.ruoyi.securities.service;
+
+import java.util.List;
+import com.ruoyi.securities.domain.TbSecuritiesData;
+
+/**
+ * 证劵交易Service接口
+ * 
+ * @author linhf
+ * @date 2025-03-01
+ */
+public interface ITbSecuritiesDataService 
+{
+    /**
+     * 查询证劵交易
+     * 
+     * @param id 证劵交易主键
+     * @return 证劵交易
+     */
+    public TbSecuritiesData selectTbSecuritiesDataById(Long id);
+
+    /**
+     * 查询证劵交易列表
+     * 
+     * @param tbSecuritiesData 证劵交易
+     * @return 证劵交易集合
+     */
+    public List<TbSecuritiesData> selectTbSecuritiesDataList(TbSecuritiesData tbSecuritiesData);
+
+    /**
+     * 新增证劵交易
+     * 
+     * @param tbSecuritiesData 证劵交易
+     * @return 结果
+     */
+    public int insertTbSecuritiesData(TbSecuritiesData tbSecuritiesData);
+
+    /**
+     * 修改证劵交易
+     * 
+     * @param tbSecuritiesData 证劵交易
+     * @return 结果
+     */
+    public int updateTbSecuritiesData(TbSecuritiesData tbSecuritiesData);
+
+    /**
+     * 批量删除证劵交易
+     * 
+     * @param ids 需要删除的证劵交易主键集合
+     * @return 结果
+     */
+    public int deleteTbSecuritiesDataByIds(Long[] ids);
+
+    /**
+     * 删除证劵交易信息
+     * 
+     * @param id 证劵交易主键
+     * @return 结果
+     */
+    public int deleteTbSecuritiesDataById(Long id);
+}
